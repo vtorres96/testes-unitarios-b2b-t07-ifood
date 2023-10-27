c6d197bd8f8a8e5e1b61562921d201743ea15eb6
Victor Torres <victor.torres_taking@totalexpress.com.br>
2023-10-27 20:31:25 -0300

    ajustando projeto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-const express = require('express');
-const rotas = require('./rotas');
+const app = require('./servidor');
 
-const app = express();
-
-app.use(express.json());
-
-app.use(rotas);
-
-app.listen(3000)
+app.listen(3000);
